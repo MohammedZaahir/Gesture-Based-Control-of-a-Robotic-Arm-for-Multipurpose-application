a55c76c077ddc8b6115d1b2dd77c75bb324f2919
MohammedZaahir <151950035+MohammedZaahir@users.noreply.github.com>
2024-10-07 17:55:57 +0400

    arduinocode.ino

--- arduinocode.ino
+++ arduinocode.ino
@@ -1,69 +1,77 @@
 #include <Servo.h>
 
-// Create Servo objects for the four servos
+// Creating Servo objects for controlling the four motors
 Servo servo1, servo2, servo3, servo4;
 
-int angles[4] = {0, 0, 0, 0};  // Array to hold angles for the four servos
-String inputString = "";  // String to hold incoming data
+int angles[4] = {0, 0, 0, 0};  // This array will store the angles for the four servos
+String inputString = "";  // I use this to hold incoming serial data as a string
 
 void setup() {
-  // Attach servos to respective pins
+  // Attaching the servos to their respective pins
   servo1.attach(9);
   servo2.attach(10);
   servo3.attach(11);
   servo4.attach(12);
 
-  // Begin serial communication at 9600 baud
+  // Setting up the serial communication so we can send data from Python (9600 baud rate)
   Serial.begin(9600);
 
-  // Initialize all servos to 0 degrees
+  // Setting all servos to 0 degrees at startup
   resetServos();
 }
 
 void loop() {
-  // Read incoming data
+  // Checking if there's any data coming from the serial port
   if (Serial.available()) {
-    char c = (char)Serial.read();
+    char c = (char)Serial.read();  // Reading each character from the incoming data
     if (c == '\n') {
-      processInputString();  // Process the string when newline detected
-      inputString = "";  // Clear the string after processing
+      // If newline detected, process the entire string and reset it
+      processInputString();
+      inputString = "";  // Clear the string after handling the data
     } else {
-      inputString += c;  // Append the character to input string
+      inputString += c;  // Keep adding characters to the string until a newline is found
     }
   }
 }
 
 void processInputString() {
-  // Parse the input string into servo angles
-  int indices[3] = {inputString.indexOf(','), inputString.indexOf(',', inputString.indexOf(',') + 1), inputString.indexOf(',', inputString.indexOf(',', inputString.indexOf(',') + 1) + 1)};
+  // I'm parsing the string here to break it into four parts representing the servo angles
+  int indices[3] = {
+    inputString.indexOf(','),  // Finding first comma
+    inputString.indexOf(',', inputString.indexOf(',') + 1),  // Second comma
+    inputString.indexOf(',', inputString.indexOf(',', inputString.indexOf(',') + 1) + 1)  // Third comma
+  };
 
-  if (indices[2] > 0) {
+  if (indices[2] > 0) {  // Only proceed if all three commas were found
+    // Extracting the angles from the string and converting them to integers
     angles[0] = inputString.substring(0, indices[0]).toInt();
     angles[1] = inputString.substring(indices[0] + 1, indices[1]).toInt();
     angles[2] = inputString.substring(indices[1] + 1, indices[2]).toInt();
     angles[3] = inputString.substring(indices[2] + 1).toInt();
 
+    // Move the servos based on these angles
     moveServos();
   }
 }
 
 void moveServos() {
-  // Move the servos to the respective angles
+  // Sending the calculated angles to each of the servos
   servo1.write(angles[0]);
   servo2.write(angles[1]);
   servo3.write(angles[2]);
   servo4.write(angles[3]);
 
-  // Optional: Print angles for debugging
+  // Printing the angles to the serial monitor for debugging purposes
   Serial.print("Servo Angles: ");
   for (int i = 0; i < 4; i++) {
     Serial.print(angles[i]);
-    if (i < 3) Serial.print(", ");
+    if (i < 3) Serial.print(", ");  // Add commas between angles except for the last one
   }
-  Serial.println();
+  Serial.println();  // Move to the next line after printing all angles
 }
 
 void resetServos() {
+  // Resets all servos to 0 degrees during initialization
   servo1.write(0);
   servo2.write(0);
   servo3.write(0);
